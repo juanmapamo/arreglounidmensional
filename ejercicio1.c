3469de157691cf41f0a13d8fa9e6827c5c9f2a7b
Laboratorio <juan.patino@udla.edu.ec>
2023-11-15 15:17:07 -0500

    Segunda  linea de codigo

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main (void)
 {
-    int vector[10];
-    
-    printf ()
-    
-    
+    int vector1[10];
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Ingresa el contenido del vector en la posicion %d.\n", i+1);
+        scanf ("%d", &vector1[1]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d,  ", vector1 [i]);
+    }
     
     return 0;
 }
